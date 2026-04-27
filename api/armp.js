b4949d6361057aac4b6aee91a99349985ba58b5a
saeedsony <saeedsonywow@gmail.com>
2026-04-27 11:01:06 +0330

    Update armp.js

--- api/armp.js
+++ api/armp.js
@@ -1,25 +1,29 @@
 export default async function handler(req, res) {
-const target = "[http://](http://81.91.176.222:443/armp)[81.91.176.222](http://81.91.176.222:443/armp)[:443/armp](http://81.91.176.222:443/armp)";
+  const target = "http://81.91.176.222:443/armp";
 
-try {
-const response = await fetch(target, {
-method: req.method,
-headers: req.headers,
-body: req.method !== "GET" && req.method !== "HEAD" ? req.body : undefined,
-});
+  try {
+    const response = await fetch(target, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: "81.91.176.222",
+      },
+      body: req.method === "GET" || req.method === "HEAD"
+        ? undefined
+        : req.body,
+    });
 
-```
-res.status(response.status);
+    res.status(response.status);
 
-response.headers.forEach((value, key) => {
-  res.setHeader(key, value);
-});
+    response.headers.forEach((value, key) => {
+      if (!["content-encoding", "transfer-encoding", "connection"].includes(key.toLowerCase())) {
+        res.setHeader(key, value);
+      }
+    });
 
-const data = await response.arrayBuffer();
-res.send(Buffer.from(data));
-```
-
-} catch (err) {
-res.status(500).send("Proxy Error");
-}
+    const data = await response.arrayBuffer();
+    res.send(Buffer.from(data));
+  } catch (err) {
+    res.status(500).send("Proxy Error");
+  }
 }
